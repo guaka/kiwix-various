b8a5282033cc109423dc52c719a262c03bc77d23
Kasper Souren <kasper@guaka.org>
2014-11-12 17:52:17 +0100

    usbstick-init.sh

---
+++ usbstick-init.sh
@@ -0,0 +1,5 @@
+/usr/sbin/diskutil rename KINGSTON AFRIPEDIA
+wget http://download.kiwix.org/bin/0.9/kiwix-0.9-win.zip
+wget http://download.kiwix.org/bin/0.9/kiwix-0.9.dmg
+wget https://sourceforge.net/projects/kiwix/files/
+wget http://softlayer-ams.dl.sourceforge.net/project/kiwix/0.9/kiwix-0.9-linux-i686.tar.bz2
